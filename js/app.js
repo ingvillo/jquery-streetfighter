2c758a3276030f3bdb28ade16d8d9c32dff5a83c
ingvillo <ingvilloddsen@gmail.com>
2015-08-04 11:00:16 +0200

    Added instructions and looking cool animation

--- js/app.js
+++ js/app.js
@@ -2,17 +2,20 @@ $(document).ready(function(){
  
 	$('.ryu').mouseenter(function(){
  		$('.ryu-still').hide();
+ 		$('.ryu-cool').hide();
  		$('.ryu-ready').show();
  	})
 
 	.mouseleave(function(){
  		$('.ryu-ready').hide();
+ 		$('.ryu-cool').hide();
  		$('.ryu-still').show();
  	})
 
 	.mousedown(function(){
    		playHadouken();
  		$('.ryu-ready').hide();
+ 		$('.ryu-cool').hide();
  		$('.ryu-throwing').show();
  		$('.hadouken').finish().show()
  		.animate(
@@ -20,20 +23,42 @@ $(document).ready(function(){
  			500,
  			function() {
  				$(this).hide();
- 				$(this).css('left', '20px');
+ 				$(this).css('left', '-180px');
  			}
  			);
  	})
 
 	.mouseup(function(){
 		$('.ryu-throwing').hide();
+		$('ryu-cool').hide();
+		$('ryu-still').hide();
 		$('.ryu-ready').show();
 	});
+
 
 	function playHadouken () {
 		$('#hadouken-sound')[0].volume=0.5;
 		$('#hadouken-sound')[0].load();
 		$('#hadouken-sound')[0].play();
-	}
+	};
+
+	$(document).keydown(function(event){
+    	if (event.which == 88){
+      		$('.ryu-ready').hide();
+      		$('.ryu-still').hide();
+      		$('ryu-throwing').hide();
+      		$('.ryu-cool').show();
+    	}
+	})
+
+	.keyup(function(event){
+    	if (event.which == 88){
+     		$('.ryu-cool').hide();
+      		$('.ryu-ready').hide();
+      		$('.ryu-throwing').hide();
+     		$('.ryu-still').show();
+      	}
+  	});
+
 
 });
