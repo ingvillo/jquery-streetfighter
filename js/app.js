7fa7b7fc10916e366ca5e02b2afa31a52e72594e
ingvillo <ingvilloddsen@gmail.com>
2015-08-11 07:31:31 +0200

    fix duplicate image

--- js/app.js
+++ js/app.js
@@ -3,23 +3,26 @@ $(document).ready(function(){
 	$('.ryu').mouseenter(function(){
  		$('.ryu-still').hide();
  		$('.ryu-cool').hide();
+ 		$('.ryu-throwing').hide();
  		$('.ryu-ready').show();
  	})
 
 	.mouseleave(function(){
  		$('.ryu-ready').hide();
  		$('.ryu-cool').hide();
+ 		$('.ryu-throwing').hide();
  		$('.ryu-still').show();
  	})
 
 	.mousedown(function(){
    		playHadouken();
+   		$('.ryu-still').hide();
  		$('.ryu-ready').hide();
  		$('.ryu-cool').hide();
  		$('.ryu-throwing').show();
  		$('.hadouken').finish().show()
  		.animate(
- 			{'left': '1020px'},
+ 			{'left': '500px'},
  			500,
  			function() {
  				$(this).hide();
@@ -30,14 +33,14 @@ $(document).ready(function(){
 
 	.mouseup(function(){
 		$('.ryu-throwing').hide();
-		$('ryu-cool').hide();
-		$('ryu-still').hide();
-		$('.ryu-ready').show();
+		$('.ryu-cool').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
 	});
 
 
 	function playHadouken () {
-		$('#hadouken-sound')[0].volume=0.5;
+		$('#hadouken-sound')[0].volume=0.3
 		$('#hadouken-sound')[0].load();
 		$('#hadouken-sound')[0].play();
 	};
@@ -46,7 +49,7 @@ $(document).ready(function(){
     	if (event.which == 88){
       		$('.ryu-ready').hide();
       		$('.ryu-still').hide();
-      		$('ryu-throwing').hide();
+      		$('.ryu-throwing').hide();
       		$('.ryu-cool').show();
     	}
 	})
